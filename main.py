0c983c3a46aa9f94bbb654e56efbcbbf5b4cf1f5
Michael Auger <michaelauger@Michaels-MBP.attlocal.net>
2019-02-28 11:47:31 -0600

    Commit data files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from problem_solver import ProblemSolver
 
 # Input
 
-files = ['a_example.in', 'b_small.in', 'c_medium.in', 'd_big.in']
+files = ['a_example.txt', 'b_loveley_landscapes.txt', 'c_memorable_moments.txt', 'd_pet_pictures.txt', 'e_shiny_selfies.txt']
 header_rows = ['rows', 'cols', 'minIngredients', 'maxCellsPerSlice']
 col_headers = ['row']
 
@@ -13,21 +13,25 @@ col_headers = ['row']
 
 example_file = os.path.join('data', files[0])
 example = read_file(example_file, header_rows, col_headers)
-exampleSlicer = ProblemSolver(example['header'], example['rows'], os.path.join('output', 'example.out'))
+exampleSlicer = ProblemSolver(example['header'], example['rows'], os.path.join('output', 'example.txt'))
 exampleSlicer.output_solution()
 
 small_file = os.path.join('data', files[1])
 small = read_file(small_file, header_rows, col_headers)
-smallSlicer = ProblemSolver(small['header'], small['rows'], os.path.join('output', 'small.out'))
+smallSlicer = ProblemSolver(small['header'], small['rows'], os.path.join('output', 'small.txt'))
 smallSlicer.output_solution()
 
 medium_file = os.path.join('data', files[2])
 medium = read_file(medium_file, header_rows, col_headers)
-mediumSlicer = ProblemSolver(medium['header'], medium['rows'], os.path.join('output', 'medium.out'))
+mediumSlicer = ProblemSolver(medium['header'], medium['rows'], os.path.join('output', 'medium.txt'))
 mediumSlicer.output_solution()
 
 big_file = os.path.join('data', files[3])
 big = read_file(big_file, header_rows, col_headers)
-bigSlicer = ProblemSolver(big['header'], big['rows'], os.path.join('output', 'big.out'))
+bigSlicer = ProblemSolver(big['header'], big['rows'], os.path.join('output', 'big.txt'))
 bigSlicer.output_solution()
 
+biggest_file = os.path.join('data', files[3])
+biggest = read_file(biggest_file, header_rows, col_headers)
+biggestSlicer = ProblemSolver(biggest['header'], biggest['rows'], os.path.join('output', 'big.txt'))
+biggestSlicer.output_solution()
